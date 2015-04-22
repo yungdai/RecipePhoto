20ec1e3cafc59c2ee850d0290f297dd85dba9299
Yung Dai <yungchidai@gmail.com>
2015-04-22 12:37:34 -0400

    commit

---
+++ RecipeCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  RecipeCollectionViewCell.m
+//  RecipePhoto
+//
+//  Created by Yung Dai on 2015-04-22.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import "RecipeCollectionViewCell.h"
+
+@implementation RecipeCollectionViewCell
+
+@end
