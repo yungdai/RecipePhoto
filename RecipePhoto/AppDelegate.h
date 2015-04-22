7542a1bdfc288591a49a2300675043989cb8b571
Yung Dai <yungchidai@gmail.com>
2015-04-22 11:30:55 -0400

    Initial Commit

---
+++ RecipePhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecipePhoto
+//
+//  Created by Yung Dai on 2015-04-22.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RecipePhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecipePhoto
+//
+//  Created by Yung Dai on 2015-04-22.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
