bce08f2e32f4ce2e40d45cfe4537532bf52f7e8a
Yung Dai <yungchidai@gmail.com>
2015-04-27 14:56:30 -0400

    Completed Chapter 16

--- RecipeCollectionViewController.m
+++ RecipeCollectionViewController.m
@@ -9,6 +9,9 @@
 #import "RecipeCollectionViewController.h"
 // import the RecipeCollecitonView Model to the RecipeCollectionViewController
 #import "RecipeCollectionViewCell.h"
+// import the RecipeCollectionHeaderView Model to the RecipeCollectionViewController
+#import "RecipeCollectionHeaderView.h"
+
 
 @interface RecipeCollectionViewController ()
 
@@ -28,32 +31,66 @@ static NSString * const reuseIdentifier = @"Cell";
     // Register cell classes
     // [self.collectionView registerClass:[RecipeCollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
     // Initialize image array
-    recipeImages = @[@"angry_birds_cake.jpg", @"creme_brelee.jpg", @"egg_benedict.jpg",
-                     @"full_breakfast.jpg", @"green_tea.jpg", @"ham_and_cheese_panini.jpg",
-                     @"ham_and_egg_sandwich.jpg", @"hamburger.jpg", @"instant_noodle_with_egg.jpg",  @"japanese_noodle_with_pork.jpg", @"mushroom_risotto.jpg", @"noodle_with_bbq_pork.jpg", @"starbucks_coffee.jpg", @"thai_shrimp_cake.jpg", @"vegetable_curry.jpg", @"white_chocolate_donut.jpg"];
+    NSArray *mainDishImages = @[@"egg_benedict.jpg", @"full_breakfast.jpg",
+                                @"ham_and_cheese_panini.jpg", @"ham_and_egg_sandwich.jpg", @"hamburger.jpg",
+                                @"instant_noodle_with_egg.jpg", @"japanese_noodle_with_pork.jpg",
+                                @"mushroom_risotto.jpg", @"noodle_with_bbq_pork.jpg", @"thai_shrimp_cake.jpg",
+                                @"vegetable_curry.jpg"];
+    NSArray *drinkDessertImages = @[@"angry_birds_cake.jpg", @"creme_brelee.jpg",
+                                    @"green_tea.jpg", @"starbucks_coffee.jpg", @"white_chocolate_donut.jpg"];
+    
+    // recipeImages array now has arrays inside the array
+    recipeImages = @[mainDishImages, drinkDessertImages];
+    
+    // adding section spacing
+    UICollectionViewFlowLayout *collectionViewLayout =  (UICollectionViewFlowLayout *) self.collectionViewLayout;
+    collectionViewLayout.sectionInset = UIEdgeInsetsMake(20, 0, 20, 0);
+
 }
 
+// implement the viewForSupplementaryElementOfKind: method
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
+    
+    UICollectionReusableView *reusableView = nil;
+    
+    if (kind == UICollectionElementKindSectionHeader) {
+        RecipeCollectionHeaderView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"HeaderView" forIndexPath:indexPath];
+        NSString *title = [[NSString alloc]initWithFormat:@"Recipe Group #%ld", indexPath.section +1];
+        headerView.titleLabel.text = title;
+        reusableView = headerView;
+        
+    }
+    
+    if (kind == UICollectionElementKindSectionFooter) {
+        UICollectionReusableView *footerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:@"FooterView" forIndexPath:indexPath];
+        reusableView = footerView;
+    }
+    return reusableView;
+}
 
 
 #pragma mark <UICollectionViewDataSource>
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
 
-    return 1;
+    return [recipeImages count];
 }
 
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-
-    return recipeImages.count   ;
+    // method to return the number of items for each section in the array within the array
+    return [[recipeImages objectAtIndex:section]count];
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     RecipeCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
 
     // Configure the cell
-    cell.recipeImageView.image = [UIImage imageNamed:[recipeImages objectAtIndex:indexPath.row]];
     cell.backgroundView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"photo-frame"]];
+    
+    // rewrite of the cell image view into groups
+    cell.recipeImageView.image = [UIImage imageNamed:[recipeImages[indexPath.section]objectAtIndex:indexPath.row]];
+    
     return cell;
 }
 
